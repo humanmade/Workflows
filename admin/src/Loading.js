098bd18f24a922d39defc0ae56fa69ca08c7aa6d
CircleCI <rob+platform-ui-build@hmn.md>
2018-05-14 14:00:03 +0000

    Build changes from 7c72ab5d8dd68a339b6de4f018b5452d61b1bf53
    https://circleci.com/gh/humanmade/Workflows/9

---
+++ admin/src/Loading.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import __ from './l10n';
+
+const StyledLoading = styled.div`
+	.spinner {
+		display: block;
+		float: none;
+		margin: 30px auto;
+	}
+`
+
+const Loading = ({ isLoading, error }) => {
+	if ( isLoading ) {
+		return <StyledLoading>
+			<span className="spinner is-active" />
+		</StyledLoading>;
+	}
+
+	if ( error ) {
+		return <StyledLoading>
+			<div className="notice notice-error"><p>{__( 'There was an error loading the plugin UI.' )}</p></div>
+		</StyledLoading>;
+	}
+
+	return null;
+};
+
+export default Loading;
