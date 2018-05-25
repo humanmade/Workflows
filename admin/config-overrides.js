33f79d36197fb168420b65456573ef5851bc3b48
CircleCI <rob+platform-ui-build@hmn.md>
2018-05-25 07:49:10 +0000

    Build changes from ef614ce570e39c5586abafddface5eef173f644f
    https://circleci.com/gh/humanmade/Workflows/18

---
+++ admin/config-overrides.js
@@ -0,0 +1,26 @@
+const { compose } = require( 'react-app-rewired' );
+const rewireStyledComponents = require( 'react-app-rewire-styled-components' );
+const rewireSVG = require( 'react-app-rewire-svg-react-loader' );
+const DynamicPublicPathPlugin = require('dynamic-public-path-webpack-plugin');
+
+//  custom config
+module.exports = function ( config, env ) {
+	const rewires = compose(
+		rewireStyledComponents,
+		rewireSVG,
+	);
+
+	config.plugins.push( new DynamicPublicPathPlugin( {
+		externalGlobal: 'window.HM.Workflows.BuildURL', //Your global variable name.
+		chunkName: 'hm-workflows',
+	} ) );
+
+	config.entry = { 'hm-workflows': config.entry };
+
+	config.output.jsonpFunction = 'hmWorkflowsJSONP';
+
+	// Isolate styled components.
+	process.env.SC_ATTR = 'hm-workflows';
+
+	return rewires( config, env );
+}
