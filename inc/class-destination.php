90d4eab36b2ad1fabb6509afd365a14b20d8e7fb
Chris Reynolds <chris@humanmade.com>
2020-05-05 11:57:41 -0600

    add space before colon for cs

--- inc/class-destination.php
+++ inc/class-destination.php
@@ -53,7 +53,7 @@ class Destination {
 	 *
 	 * @return Destination
 	 */
-	public static function register( string $id, callable $handler ): Destination {
+	public static function register( string $id, callable $handler ) : Destination {
 		$destination            = new self( $id, $handler );
 		self::$instances[ $id ] = $destination;
 
@@ -105,7 +105,7 @@ class Destination {
 	 * @param callable $handler
 	 * @return $this
 	 */
-	public function set_handler( callable $handler ): Destination {
+	public function set_handler( callable $handler ) : Destination {
 		$this->handler = $handler;
 
 		return $this;
@@ -145,7 +145,7 @@ class Destination {
 	 *
 	 * @return $this
 	 */
-	public function add_ui( $ui ): Destination {
+	public function add_ui( $ui ) : Destination {
 		if ( is_string( $ui ) ) {
 			$this->ui = UI::register( $ui );
 		}
