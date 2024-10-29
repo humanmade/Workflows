811fca086204a4a31762e5e642cdfab2eca75c07
Zeshan Ahmed <zahmed@greenstreet.com>
2024-10-29 11:11:26 +0000

    Add support for single user value if users select is not set to multi

--- admin/src/WorkflowUI.js
+++ admin/src/WorkflowUI.js
@@ -570,7 +570,11 @@ class WorkflowUI extends Component {
 									.then( response => response.json() )
 									.then( data => ({ options: data }) )
 								}
-								value={(recipient.users || []).filter(user => recipient.value.includes(String(user.id)))}
+								value={
+									recipient.multi
+										? ( recipient.users || [] ).filter( user => recipient.value.includes( String( user.id ) ) )
+										: recipient.users
+								}
 								labelKey={recipient.endpoint.labelKey || 'name'}
 								valueKey={recipient.endpoint.valueKey || 'id'}
 								onChange={option => this.setState( {
@@ -579,24 +583,27 @@ class WorkflowUI extends Component {
 											return rec;
 										}
 
-										const selectedValue = rec.multi
-											? option.map(opt => String(opt[recipient.endpoint.valueKey || 'id']))
-											: String(option[recipient.endpoint.valueKey || 'id']);
+										// Make sure to only set value if option is not null or empty.
+										const selectedValue = option ?
+											rec.multi
+												? option.map(opt => String(opt[recipient.endpoint.valueKey || 'id']))
+												: String( option[recipient.endpoint.valueKey || 'id'] )
+											: '';
 
 										// Handle single or multi-select options
 										const selectedUsers = rec.multi ? option : [option];
 
 										// Merge with the existing users
-										const mergedItems = [...(rec.users || []), ...(selectedUsers || [])].reduce((acc, current) => {
-											if (!acc.some(item => item.id === current.id)) {
-												acc.push(current);
+										const mergedItems = [...(rec.users || []), ...( selectedUsers || [])].reduce( ( acc, current ) => {
+											if ( ! acc.some(item => item.id === current.id ) ) {
+												acc.push( current );
 											}
 											return acc;
-										}, []);
+										}, [] );
 
 										return Object.assign({}, rec, {
 											value: selectedValue,
-											users: mergedItems
+											users: rec.multi ? mergedItems : mergedItems[0]
 										});
 									})
 								})}
